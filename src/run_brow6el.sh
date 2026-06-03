ed8650f1ae2b4c92846e8f8cf8465080123887f3
Malcolm Ke Win <kexianbin@diyism.com>
2026-06-04 02:09:48 +0800

    Update run_brow6el.sh

--- src/run_brow6el.sh
+++ src/run_brow6el.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get the directory where this script is located
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="/opt/brow6el"
 
 # Set library path to use local libcef.so
 export LD_LIBRARY_PATH="$DIR:$LD_LIBRARY_PATH"
